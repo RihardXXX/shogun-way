f7ce7d9bbe9de5b400cb7f1e76488b248b35e82c
RihardXXX <yusupa.akaeva@yandex.ru>
2020-02-26 20:40:18 +0300

    base commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSW4DcKPEofhjH8NpGEeyR6uwnUAXlJnF2zpFyOey0sKlXDZQmK"
+                alt=""/>
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <div><a href="#">Профиль</a></div>
+            <div><a href="#">Сообщения</a></div>
+            <div><a href="#">Настройки</a></div>
+            <div><a href="#">Выйти</a></div>
+        </nav>
+    )
+};
+
+export default Navbar;
