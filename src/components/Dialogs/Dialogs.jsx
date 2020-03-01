347717cd8040330dab27d6939dfcf43c0ec858fc
RihardXXX <yusupa.akaeva@yandex.ru>
2020-03-02 01:49:48 +0300

    create dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import d from './Dialogs.module.css'
+import p from "../Profile/MyPosts/MyPosts.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            <h3>It is pages Dialogs</h3>
+        <div className={d.dialogs}>
+            <div className={d.dialogItem}>
+                <div className={d.dialog + ' ' + d.active}>Саня</div>
+                <div className={d.dialog}>Виктор</div>
+                <div className={d.dialog}>Виктор</div>
+                <div className={d.dialog}>Виктор</div>
+                <div className={d.dialog}>Виктор</div>
+            </div>
+            <div className={d.messagesItem}>
+                <div className={d.message + ' ' + d.active}><h3>message</h3></div>
+                <button className={p.button}>отправить сообщение</button>
+            </div>
         </div>
     );
 };
